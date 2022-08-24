9ed9a26012329c259ae51ec9aa7299cce53ded97
Максим Кучишкин <kuchishkin.m@gmail.com>
2022-08-24 10:16:23 +0300

    update

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -13,7 +13,7 @@ router.register('ingredients', IngredientsViewSet, basename='ingredients')
 router.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
-    path('api/auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.authtoken')),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
