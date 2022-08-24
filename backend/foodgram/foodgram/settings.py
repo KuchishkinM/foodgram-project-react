6e97e45a6788f7362ec7003116ead7391575db65
Максим Кучишкин <kuchishkin.m@gmail.com>
2022-08-24 14:18:53 +0300

    update

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -128,26 +128,49 @@ USE_L10N = True
 USE_TZ = True
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
         'rest_framework.authentication.TokenAuthentication',
+
+    ),
+
+    'DEFAULT_PERMISSION_CLASSES': [
+
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+
     ],
-    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+
+    'PAGE_SIZE': 6,
+
 }
 
 DJOSER = {
-    'SERIALIZERS': {
-        'user': 'api.serializers.UserSerializer',
-        'user_create': 'api.serializers.UserSerializer',
-        'current_user': 'api.serializers.UserSerializer',
-    },
-    'PERMISSIONS': {
-        'user_list': ('rest_framework.permissions.AllowAny',),
-        'user': ('rest_framework.permissions.AllowAny',),
-    },
+
+    'LOGIN_FIELD': 'email',
+
     'HIDE_USERS': False,
+
+    'SERIALIZERS': {
+
+        'user_create': 'api.serializers.CustomUserCreateSerializer',
+
+        'user': 'api.serializers.CustomUserSerializer',
+
+        'current_user': 'api.serializers.CustomUserSerializer',
+
+    },
+
+    'PERMISSIONS': {
+
+        'user': ('djoser.permissions.CurrentUserOrAdminOrReadOnly',),
+
+        'user_list': ('rest_framework.permissions.AllowAny',)
+
+    }
+
 }
 
 
