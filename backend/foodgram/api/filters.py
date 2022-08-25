d40905ec596e431a9feea9f41879a500eb34facc
Максим Кучишкин <kuchishkin.m@gmail.com>
2022-08-25 16:21:53 +0300

    fixing

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,31 +1,33 @@
-from django.contrib.auth import get_user_model
+from django.db.models import QuerySet
 from django_filters import FilterSet, filters
-from recipes.models import Recipe
 from rest_framework.filters import SearchFilter
 
-User = get_user_model()
+from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(FilterSet):
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
-    is_favorited = filters.NumberFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.NumberFilter(
-        method='get_is_in_shopping_cart'
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        queryset=Tag.objects.all(),
+        to_field_name='slug',
+    )
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart'
     )
 
     class Meta:
         model = Recipe
-        fields = ('is_favorited', 'is_in_shopping_cart', 'author', 'tags')
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
 
-    def get_is_favorited(self, queryset, name, value):
-        if value and not self.request.user.is_anonymous:
+    def filter_is_favorited(self, queryset, name, value) -> QuerySet:
+        if value:
             return queryset.filter(favorites__user=self.request.user)
         return queryset
 
-    def get_is_in_shopping_cart(self, queryset, name, value):
-        if value and not self.request.user.is_anonymous:
-            return queryset.filter(cart__user=self.request.user)
+    def filter_is_in_shopping_cart(self, queryset, name, value) -> QuerySet:
+        if value:
+            return queryset.filter(carts__user=self.request.user)
         return queryset
 
 
--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -19,6 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
+    'django_filters',
     'api',
     'users',
     'recipes',
