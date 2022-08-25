53b40b5de50c34c1510f92d23ffb09449f79b09c
Максим Кучишкин <kuchishkin.m@gmail.com>
2022-08-25 16:30:52 +0300

    fixing

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,33 +1,31 @@
-from django.db.models import QuerySet
+from django.contrib.auth import get_user_model
 from django_filters import FilterSet, filters
+from recipes.models import Recipe
 from rest_framework.filters import SearchFilter
 
-from recipes.models import Recipe, Tag
+User = get_user_model()
 
 
 class RecipeFilter(FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        queryset=Tag.objects.all(),
-        to_field_name='slug',
-    )
-    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_is_in_shopping_cart'
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    is_favorited = filters.NumberFilter(method='get_is_favorited')
+    is_in_shopping_cart = filters.NumberFilter(
+        method='get_is_in_shopping_cart'
     )
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+        fields = ('is_favorited', 'is_in_shopping_cart', 'author', 'tags')
 
-    def filter_is_favorited(self, queryset, name, value) -> QuerySet:
-        if value:
+    def get_is_favorited(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
             return queryset.filter(favorites__user=self.request.user)
         return queryset
 
-    def filter_is_in_shopping_cart(self, queryset, name, value) -> QuerySet:
-        if value:
-            return queryset.filter(carts__user=self.request.user)
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(cart__user=self.request.user)
         return queryset
 
 
